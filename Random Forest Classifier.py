5c9da1bc1d369e5478f392d26a275b98aba98406
Sumit Chaturvedi <119087961+CustomBooch9236@users.noreply.github.com>
2022-11-26 03:27:48 +0000

    Create Random Forest Classifier.py

---
+++ Random Forest Classifier.py
@@ -0,0 +1,26 @@
+import panads as pd
+
+# Collecting Data
+import pandas as pd
+exo_train_df = pd.read_csv('https://student-datasets-bucket.s3.ap-south-1.amazonaws.com/whitehat-ds-datasets/kepler-exoplanets-dataset/exoTrain.csv')
+exo_test_df = pd.read_csv('https://student-datasets-bucket.s3.ap-south-1.amazonaws.com/whitehat-ds-datasets/kepler-exoplanets-dataset/exoTest.csv')
+
+# Importing the 'RandomForestClassifier' Module from the 'sklearn.ensemble' Library
+from sklearn.ensemble import RandomForestClassifier
+
+# Extracting the Feature Variables from the Training Dataset
+x_train = exo_train_df.iloc[:,1:]
+y_train = exo_train_df.iloc[:,0]
+
+# Fitting the Model
+#1 Calling the 'RandomForestClassifier' Module with Inputs as 'n_jobs = - 1' & 'n_estimators=50'
+rf_clf = RandomForestClassifier(n_jobs=-1, n_estimators=50)
+#2 Calling the 'fit()' Function with 'x_train' and 'y_train' as Inputs\
+print(rf_clf.fit(x_train,y_train))
+#3 Calling the 'score()' Function with 'x_train' and 'y_train' as Inputs to Check the Accuracy Score of the Model
+print(rf_clf.score(x_train,y_train))
+
+# The predict() Function
+x_test = exo_test_df.iloc[:,1:] #extracting the feature variables
+y_test = exo_test_df.iloc[:,0] #extracting the target variable
+y_predicted = rf_clf.predict(x_test)
